04e796a8a68cfdcf4fe934a95ad56933390bd3e8
vovik <vk@kitworks.systems>
2018-11-19 17:49:18 +0200

    update

--- jaunt/individual.php
+++ jaunt/individual.php
@@ -1,7 +1,7 @@
 <?php
 require_once('../ripcordr/ripcord.php');
 
-function insert_registration($server, $r) { 
+function insert_order($server, $r) { 
     $info = ripcord::client($server['url'])->start();
     $common = ripcord::client($server['url'].'common');
     $uid = $common->authenticate($server['db'], $server['username'], $server['password'], array());
@@ -112,50 +112,6 @@ function insert_registration($server, $r) {
     // отримання ID мови туру  -- завершення
 
 
-    // отримання ID гіду туру -- початок
-    if ($r['jaunt']['guide'] and is_array($r['jaunt']['guide'])){
-        $models = ripcord::client($server['url'].'object');
-        $external_ids = $models->execute_kw($server['db'], $uid, $server['password'],
-                'ir.model.data', 'search', array(
-                    array(
-                        array('module', '=', "site_guide_code"),
-                        array('model', '=', "res.partner"),
-                        array('name', '=', $r['jaunt']['guide']['id']))));
-        if ($external_ids) {
-            $external_id = $models->execute_kw($server['db'], $uid, $server['password'],
-                'ir.model.data', 'read', array($external_ids));
-            $guide_id = $external_id[0]['res_id'];                        
-        } else { 
-            $guide = $models->execute_kw($server['db'], $uid, $server['password'],
-                'res.partner', 'search', array(
-                    array(array('name', '=', $r['jaunt']['guide']['name']))));        
-            if(count($lang)) {
-                $guide_id = $guide[0];
-            } 
-            else {
-                $guide_id = $models->execute_kw($server['db'], $uid, $server['password'],
-                    'res.partner', 'create', array(
-                        array('name'=>$r['jaunt']['guide']['name'])));
-            }
-            $models->execute_kw($server['db'], $uid, $server['password'],
-                'ir.model.data', 'create', array(
-                    array(
-                        'module'=>"site_guide_code", 
-                        'model'=>"res.partner",
-                        'name'=>$r['jaunt']['guide']['id'],
-                        'res_id'=>$guide_id)));  
-        }    
-    } 
-    else {
-        // якщо гіда не задано, но ID не передаємо
-        $guide_id = false;
-    }
-
-    echo("guide_id: \n");
-    var_dump($guide_id);
-    // отримання ID гіду туру  -- завершення
-
-
     // отримання ID туру у каталозі -- початок
     $models = ripcord::client($server['url'].'object');
 
@@ -181,8 +137,7 @@ function insert_registration($server, $r) {
             $catalog_id = $models->execute_kw($server['db'], $uid, $server['password'],
             'jaunt.catalog', 'create', array(
                 array('name'=>$r['jaunt']['catalog']['name'],
-                    'language_ids'=>array(4,$lang_id),
-                    'guide_ids'=>array(4,$lang_id) ),
+                    'language_ids'=>array(4,$lang_id), ),
             ));
         }
         $models->execute_kw($server['db'], $uid, $server['password'],
@@ -196,6 +151,7 @@ function insert_registration($server, $r) {
     echo("catalog_id: \n");
     var_dump($catalog_id);
     // отримання ID туру у каталозі  -- завершення
+
 
 
     // отримання ID туру у розкладі -- початок
@@ -223,13 +179,20 @@ function insert_registration($server, $r) {
             $jaunt_id = $jaunt[0];
         }
         else {
+            $comment = $r['comment'];
+            if($r['is_paid']){
+                $comment = "Заказ оплачен \n". $comment;
+            }
             $jaunt_id = $models->execute_kw($server['db'], $uid, $server['password'],
             'jaunt.jaunt', 'create', array(array(
                 'catalog_id'=>$catalog_id, 
                 'date_begin'=>$r['jaunt']['date_begin'],
-                'is_individual'=>false,
+                'is_individual'=>true,
+                'organizer_id'=>$partner_id,
+                'price'=>$r['price'],
                 'language_id'=>$lang_id,
-                'guide_id'=>$guide_id),
+                'comment'=>$comment
+            ),
             ));
         } 
 
@@ -245,105 +208,4 @@ function insert_registration($server, $r) {
     var_dump($jaunt_id);
     // отримання ID туру у розкладі  -- завершення
 
-
-    // отримання ID реєстраціі на тур у розкладі -- початок
-    $models = ripcord::client($server['url'].'object');
-    // для запису контакту стільки разів, склільки він заповнив форму 
-    // використовуємо пошук по ID
-
-    $external_ids = $models->execute_kw($server['db'], $uid, $server['password'],
-        'ir.model.data', 'search', array(
-            array(array('model', '=', "jaunt.registration"),array('name', '=', $r['id']))));
-    //var_dump($registration);
-    if(count($external_ids)) {
-        $external_id = $models->execute_kw($server['db'], $uid, $server['password'],
-            'ir.model.data', 'read', array($external_ids));
-        $registration_id = $external_id[0]['res_id'];
-    }
-    else {
-        // $ticket_partner == 'bodo' значення поля партнер
-        if ($r['catalog_ticket']['partner_company']){
-            $external_ids = $models->execute_kw($server['db'], $uid, $server['password'],
-                'ir.model.data', 'search', array(
-                    array(array('model', '=', "res.partner"),
-                    array('name', '=', $r['catalog_ticket']['partner_company']['id']))));
-            if ($external_ids) {
-                $external_id = $models->execute_kw($server['db'], $uid, $server['password'],
-                    'ir.model.data', 'read', array($external_ids));
-                $ticket_partner_id = $external_id[0]['res_id'];                        
-            } else {
-                $ticket_partner_id = $models->execute_kw($server['db'], $uid, $server['password'],
-                    'res.partner', 'create', array(array(
-                        'name'=>$r['catalog_ticket']['partner_company']['name'], 
-                        'company'=>1,
-                        'jaunt_is_partner'=>1
-                    )));
-                $models->execute_kw($server['db'], $uid, $server['password'],
-                    'ir.model.data', 'create', array(array(
-                        'model'=>"res.partner", 
-                        'name'=>$r['catalog_ticket']['partner_company']['id'],
-                        'res_id'=>$ticket_partner_id
-                    )));    
-            }
-        }
-        else {
-            $ticket_partner_id = false;
-        }
-        echo("ticket_partner_id: \n");
-        var_dump($ticket_partner_id);
-
-        $stp_ids = $models->execute_kw($server['db'], $uid, $server['password'],
-            'jaunt.site.ticket.preset', 'search', array(
-                array(
-                    array('is_paid', '=', $r['is_paid']),
-                    array('ticket_type', '=', $r['catalog_ticket']['ticket_type']),
-                    array('coupon_type', '=', $r['catalog_ticket']['coupon_type']),
-                    array('partner_company_id', '=', $ticket_partner_id)
-                )));
-        echo("stp_ids: \n");
-        var_dump($stp_ids);
-        if($stp_ids){
-            $stp_id = $stp_ids[0];
-            $stp = $models->execute_kw($server['db'], $uid, $server['password'],
-                'jaunt.site.ticket.preset', 'read', array($stp_id));
-           
-            echo("stp_ids: \n");
-            var_dump($stp_ids);
-            // var_dump($stp);
-
-            $ctp_ids = $models->execute_kw($server['db'], $uid, $server['password'],
-                'jaunt.catalog.ticket', 'search', array(
-                    array(
-                        array('ticket_preset_id', '=', $stp[0]['ticket_preset_id'][0]),
-                        array('catalog_id', '=', $catalog_id)
-                    )));
-            
-            echo("ctp_ids: \n");
-            var_dump($ctp_ids);
-            $catalog_ticket_id = $ctp_ids[0];
-        }
-        else {
-            $catalog_ticket_id = false;
-        }
-                
-
-        $registration_id = $models->execute_kw($server['db'], $uid, $server['password'],
-        'jaunt.registration', 'create', array(array(
-            'jaunt_id'=>$jaunt_id, 
-            'contact_id'=>$partner_id,
-            'catalog_ticket_id'=>$catalog_ticket_id,
-            'certificate'=>$r['coupon_number'],
-            'is_paid'=>$r['is_paid'],
-            'price'=>$r['ticket_price'],
-            'visitors_qty'=>'1'),
-        ));
-        $external_ids = $models->execute_kw($server['db'], $uid, $server['password'],
-        'ir.model.data', 'create', array(array(
-            'model'=>"jaunt.registration", 
-            'name'=>$r['id'],
-            'res_id'=>$registration_id),
-        ));
-    }
-    var_dump($registration_id);
-    // отримання ID реєстраціі на тур у розкладі  -- завершення
 }
